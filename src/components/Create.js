1505fb7bf6415f654767639556e0cef98d75e8e5
aroomforimprovement <seamusart.bradley@gmail.com>
2021-09-17 12:58:41 +0100

    moved Create reducer to own file

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,9 @@
 import React, { createContext, useReducer, useContext } from 'react';
 import { Controller, SaveController } from './partials/Controller';
-import { Jumbotron, Modal } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 import { Creation } from './partials/Creation';
 import { values } from '../animator/values';
-import { CC, CONTROLS }  from '../animator/controls';
-
+import { controlReducer } from '../redux/Create';
 
 export const ControlContext = createContext(values.initialControlState);
 
@@ -13,64 +12,6 @@ export const useControlContext = () => {
 }
 
 const Create = () => {
-
-    const controlReducer = (state, action) => {
-        console.log(action.type+':'+action.data);
-        switch(action.type){
-            case 'DISABLE':{
-                return ({...state, disable: action.data});
-            }
-            case 'ENABLE':{
-                return ({...state, enable: action.data});
-            }
-            case 'MODE':{
-                return ({...state, mode: action.data});
-            }
-            case 'PC':{
-                return ({...state, pc: action.data})
-            }
-            case 'PS':{
-                return ({...state, ps: action.data});
-            }
-            case 'UNDO':{
-                return ({...state, undo: action.data});
-            }
-            case 'REDO':{
-                return ({...state, redo: action.data});
-            }
-            case 'FRAME_RATE':{
-                return ({...state, frate: action.data});
-            }
-            case 'SAVE_BG':{
-                return ({...state, saveBg: action.data});
-            }
-            case 'DRAW_BG':{
-                return ({...state, drawBg: action.data});
-            }
-            case 'WIPE':{
-                return ({...state, wipe: action.data});
-            }
-            case 'NEXT':{
-                return ({...state, next: action.data});
-            }
-            case 'DOWNLOAD':{
-                return ({...state, download: action.data});
-            }
-            case 'PREVIEW':{
-                return ({...state, 
-                    preview: action.data,
-                    });
-            }
-            case 'END_PREVIEW':{
-                return ({...state, endPreview: action.data});
-            }
-            case 'SAVE':{
-                return ({...state, save: action.data});
-            }
-            default:
-                return state;
-        }
-    }
 
     const [ controls, dispatch ] = useReducer(controlReducer, values.initialControlState);
     const controlState = { controls, dispatch };
---
+++ src/redux/Create.js
@@ -0,0 +1,71 @@
+/**
+ * This reducer takes calls from the Create Controller.
+ * The returned state is checked by the updateWithProps function
+ * in the P5 sketch. 
+ * For CHANGE controls, like penSize, penColor, the sketch updates
+ * the anim reducer with the appropriate value.
+ * For TRIGGER controls, like like enable, disable, the sketch takes
+ * a TRUE values, sends back a FALSE to this reducer (to turn off the switch)
+ * and then does an update to the anim reducer. 
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ */
+
+export const controlReducer = (state, action) => {
+    console.log(action.type+':'+action.data);
+    switch(action.type){
+        case 'DISABLE':{
+            return ({...state, disable: action.data});
+        }
+        case 'ENABLE':{
+            return ({...state, enable: action.data});
+        }
+        case 'MODE':{
+            return ({...state, mode: action.data});
+        }
+        case 'PC':{
+            return ({...state, pc: action.data})
+        }
+        case 'PS':{
+            return ({...state, ps: action.data});
+        }
+        case 'UNDO':{
+            return ({...state, undo: action.data});
+        }
+        case 'REDO':{
+            return ({...state, redo: action.data});
+        }
+        case 'FRAME_RATE':{
+            return ({...state, frate: action.data});
+        }
+        case 'SAVE_BG':{
+            return ({...state, saveBg: action.data});
+        }
+        case 'DRAW_BG':{
+            return ({...state, drawBg: action.data});
+        }
+        case 'WIPE':{
+            return ({...state, wipe: action.data});
+        }
+        case 'NEXT':{
+            return ({...state, next: action.data});
+        }
+        case 'DOWNLOAD':{
+            return ({...state, download: action.data});
+        }
+        case 'PREVIEW':{
+            return ({...state, 
+                preview: action.data,
+                });
+        }
+        case 'END_PREVIEW':{
+            return ({...state, endPreview: action.data});
+        }
+        case 'SAVE':{
+            return ({...state, save: action.data});
+        }
+        default:
+            return state;
+    }
+}
