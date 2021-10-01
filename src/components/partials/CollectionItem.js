2db25ff8c2e58372f19b2d9631b4012b924b8e70
aroomforimprovement <seamusart.bradley@gmail.com>
2021-10-01 14:10:51 +0100

    lining up collectionItem

--- src/components/partials/CollectionItem.js
+++ src/components/partials/CollectionItem.js
@@ -5,13 +5,17 @@ export const CollectionItem = ({anim}) => {
     console.log("COLLECTION ITEM: ");
     console.dir(anim);
     return(
-        <div className='container border border-black rounded'>
+        <div className='container col col-6 col-lg-8 border border-black rounded coll-item'>
             <div className='row'>
                 <img src='' alt={anim.name}
                     className='col col-3 border mt-2 ms-2'></img>
-                <h6>{anim.name}</h6>
-                <div>by {anim.username}</div>
-                <div>Created: {anim.created}</div>
+                <h5 className='col col-6 mt-2'>{anim.name}</h5>
+            </div>
+            <div className='row coll-item-detail mt-2 mb-2'>
+                <div className='col col-6'>Created: {anim.created}</div>
+                <div className='col col-6'>by {anim.username}</div>            
+                {//not sure why frames is undefined at this point
+                }
                 <div>Length: {parseFloat(anim.frames ? anim.frames.length : 1 / anim.frate).toFixed(2)}</div>
             </div>
         </div>
