eac6b8e1fa69e5fe9ea0cc9680c71226ffad07a8
aroomforimprovement <seamusart.bradley@gmail.com>
2021-10-13 14:17:33 +0100

    moved DisplayName to partials

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useReducer, useState, createContext, useContext } from 'react';
-import { SITE } from '../shared/site';
-import { Button, Form, FormGroup, Input, InputGroup } from 'reactstrap';
-import { Loading } from './partials/Loading';
+import React, { useEffect, useReducer, createContext, useContext } from 'react';
 import { Notice } from './partials/Notice';
 import { Contact } from './partials/Contact';
+import { DisplayName } from './partials/DisplayName';
 import { useMainContext } from './Main';
-import { accountReducer, getAccountInfo, deleteContact, updateDisplayName } from '../redux/Account';
+import { accountReducer, getAccountInfo } from '../redux/Account';
 
 const AccountContext = createContext({});
 
@@ -15,9 +13,8 @@ export const useAccountContext = () => {
 
 const Account = () => {
 
-    const { mainState, mainDispatch } = useMainContext();
-    const [hideNameEdit, setHideNameEdit] = useState(true);
-    const [isNameUpdating, setIsNameUpdating] = useState(false);
+    const { mainState } = useMainContext();
+
 
     const [state, dispatch] = useReducer(accountReducer, {});
     const stateOfAccount = { state, dispatch };
@@ -34,25 +31,7 @@ const Account = () => {
         }
     }
 
-    const handleEditUsername = (e) => {
-        console.log("handleEditUsername");
-        setHideNameEdit(!hideNameEdit);
-    }
-
-    const handleUpdateName = async (e) => {
-        console.log("handleUpdateName");
-        e.preventDefault();
-        setIsNameUpdating(true);
-        updateDisplayName(state.userid, e.target.displayName.value, mainState.user.access)
-        .then((name) => {
-            dispatch({type: 'SET_DISPLAY_NAME', data: name});
-            mainDispatch({type: 'SET_USERNAME', data: name});
-            setIsNameUpdating(false);
-        });
-        
-        setHideNameEdit(true);
-    }
-
+    
     const setAccountInfo = async () => {
         const id = getAccountId();
         const response = await getAccountInfo(id, mainState.user.access);
@@ -86,33 +65,7 @@ const Account = () => {
                 <AccountContext.Consumer>
                     {() => (
                         <div>
-                            <div className='row mt-4 border border-black m-2'>
-                                <div className='col-3'>
-                                    Display name:
-                                </div>
-                                <div className='col col-8'>
-                                    {isNameUpdating 
-                                    ? <Loading /> 
-                                    :
-                                    <div>
-                                        <div hidden={!hideNameEdit}><strong >{state.username}</strong></div>
-                                        <Form onSubmit={handleUpdateName} hidden={hideNameEdit}>
-                                            <FormGroup >
-                                                <InputGroup>
-                                                    <Input size='sm' type='text' name='displayName' id='displayName' defaultValue={state.username}/>
-                                                    <Button size='sm' color='secondary' onClick={handleEditUsername}>Cancel</Button>
-                                                    <Button size='sm' color='primary'>Save</Button>
-                                                </InputGroup>
-                                            </FormGroup>
-                                        </Form>
-                                    </div>
-                                    }
-                                </div>
-                                <div className='col col-1 fa fa-edit mt-1'
-                                    onClick={handleEditUsername}>
-                                    {''}
-                                </div>
-                            </div>
+                            <DisplayName />
                             <div className='row'>
                                 <div className='col col-2'>Notifications</div>
                                 <div className='col col-8'>{notices}</div>
--- src/components/partials/DisplayName.js
+++ src/components/partials/DisplayName.js
@@ -1,5 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMainContext } from '../Main';
+import { useAccountContext } from '../Account';
+import { updateDisplayName } from '../../redux/Account';
+import { Loading } from './Loading';
+import { Form, FormGroup, InputGroup, Input, Button } from 'reactstrap';
 
-const DisplayName = () => {
-    
+export const DisplayName = () => {
+
+    const [hideNameEdit, setHideNameEdit] = useState(true);
+    const [isNameUpdating, setIsNameUpdating] = useState(false);
+
+    const { mainState, mainDispatch } = useMainContext();
+    const { state, dispatch } = useAccountContext();
+    const handleEditUsername = (e) => {
+        console.log("handleEditUsername");
+        setHideNameEdit(!hideNameEdit);
+    }
+
+    const handleUpdateName = async (e) => {
+        console.log("handleUpdateName");
+        e.preventDefault();
+        setIsNameUpdating(true);
+        updateDisplayName(state.userid, e.target.displayName.value, mainState.user.access)
+        .then((name) => {
+            dispatch({type: 'SET_DISPLAY_NAME', data: name});
+            mainDispatch({type: 'SET_USERNAME', data: name});
+            setIsNameUpdating(false);
+        });
+        
+        setHideNameEdit(true);
+    }
+
+    return(
+        <div className='row mt-4 border border-black m-2'>
+                                <div className='col-3'>
+                                    Display name:
+                                </div>
+                                <div className='col col-8'>
+                                    {isNameUpdating 
+                                    ? <Loading /> 
+                                    :
+                                    <div>
+                                        <div hidden={!hideNameEdit}><strong >{state.username}</strong></div>
+                                        <Form onSubmit={handleUpdateName} hidden={hideNameEdit}>
+                                            <FormGroup >
+                                                <InputGroup>
+                                                    <Input size='sm' type='text' name='displayName' id='displayName' defaultValue={state.username}/>
+                                                    <Button size='sm' color='secondary' onClick={handleEditUsername}>Cancel</Button>
+                                                    <Button size='sm' color='primary'>Save</Button>
+                                                </InputGroup>
+                                            </FormGroup>
+                                        </Form>
+                                    </div>
+                                    }
+                                </div>
+                                <div className='col col-1 fa fa-edit mt-1'
+                                    onClick={handleEditUsername}>
+                                    {''}
+                                </div>
+                            </div>
+    );
 }
