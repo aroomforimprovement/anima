49ab8c0940f6fbcb4c55fd48aca6161d27bf9dbb
aroomforimprovement <seamusart.bradley@gmail.com>
2022-11-19 19:35:48 +0000

    deleteNotice transaction

--- api/express/controllers/collection.js
+++ api/express/controllers/collection.js
@@ -221,6 +221,7 @@ module.exports = {
         const requser = req.user.sub.replace('auth0|', '');
         console.log(`${requser} =? ${body.userid}`);
         console.log(`${requser} =? ${body.targetUserid}`);
+        console.log(`${requser} =? ${body.reqUserid}`);
         if(body.userid && typeof body.userid == 'string' && (body.userid === requser || body.targetUserid === requser || body.reqUserId === requser)){
             if((body.username && typeof body.username == 'string') || !body.username){
                 return {
