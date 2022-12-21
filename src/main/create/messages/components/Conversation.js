e3d41f093e24d2574ae1b01d322aa170c76078b9
aroomforimprovement <seamusart.bradley@gmail.com>
2022-12-21 23:49:27 +0000

    ANIM-364/extra-btn

--- src/main/create/messages/components/Conversation.js
+++ src/main/create/messages/components/Conversation.js
@@ -6,7 +6,6 @@ import { ReactP5Wrapper } from "react-p5-wrapper";
 export const Conversation = ({conversation, updateAnim}) => {
 
     const [open, setOpen] = useState(true);
-    const [thumbFiles, setThumbFiles] = useState([]);
     
     const messages = conversation?.messages?.map((message, i) => {
         console.dir(message);
@@ -14,21 +13,29 @@ export const Conversation = ({conversation, updateAnim}) => {
             <Message key={i} 
                 message={message} 
                 updateAnim={updateAnim}
-                thumbFile={thumbFiles?.[i]}
                 index={i}
             />
         )
     })
 
+    const HideButton = () => {
+        return(
+            <div>
+                {conversation?.messages?.length > 0 
+                ? <button type='button' className='btn btn-secondary btn-sm mb-1' 
+                    onClick={() => {
+                        setOpen(!open);
+                    }}>{`${open ? 'Hide' : 'See'} conversation`}</button>
+                : <Div />}
+            </div>
+        )
+    }
+
     return(
         <div className='conversation'>
+            <HideButton />
             {open ? <div>{messages}</div> : <Div/>}
-            {conversation?.messages?.length > 0 
-            ? <button type='button' className='btn btn-secondary btn-sm mb-1' 
-                onClick={() => {
-                    setOpen(!open);
-                }}>{`${open ? 'Hide' : 'See'} conversation`}</button>
-            : <Div />}
+            <HideButton />
         </div>
     )
 }
